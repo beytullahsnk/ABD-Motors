2b8570dfe811e781396d86c8dcf85c53c6a15605
Elitra <100931397+eltraore@users.noreply.github.com>
2025-02-21 16:30:51 +0100

    update achat

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
 
         try {
             await loginUser(login, password);
-            navigate('/dashboard');
+            navigate('/profile');
         } catch (error) {
             console.error('Login error:', error.response?.data);
             if (error.response?.status === 401) {
